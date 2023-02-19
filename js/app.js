1ef09801de35eef365d11d7588a407b964824f6f
shrflah96 <shrflah96@gmail.com>
2023-02-19 07:21:04 +0330

    fix : some changes

--- js/app.js
+++ js/app.js
@@ -13,4 +13,47 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop : 0},800);
         return false;
     });
+
+
+    $('.close-menu').click(function () {
+        $('.mobile-header-nav').fadeToggle()
+        $('.overlay').fadeToggle()
+    })
+    $('.mobile-header__close').click(function () {
+        $('.mobile-header-nav').fadeToggle()
+        $('.overlay').fadeToggle()
+    })
+
+    $('.mobile-categories-title').click(function () {
+        $(this).addClass('active')
+        $('.mobile-pages-title').removeClass('active')
+        $('.menu-list').css('display', 'none')
+        $('.menu-category').css('display', 'block')
+
+    })
+    $('.mobile-pages-title').click(function () {
+        $(this).addClass('active')
+        $('.mobile-categories-title').removeClass('active')
+        $('.menu-list').css('display', 'block')
+        $('.menu-category').css('display', 'none')
+
+
+    })
+
+    $('.menu-title').click(function () {
+        // $(this).parent().addClass('active')
+        $(this).parent().find('ul.menu-dropdown').toggle()
+        $(this).find('.menu-upicon').toggle()
+        $(this).find('.menu-downicon').toggle()
+
+    })
+
+    $('.cart-wrapper').click(function () {
+        $('.box-cart').fadeToggle()
+        $('.overlay').fadeToggle()
+    })
+    $('.close-cart').click(function () {
+        $('.box-cart').fadeToggle()
+        $('.overlay').fadeToggle()
+    })
 })
