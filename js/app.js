ca65cbb675c827b49319af25221f85f0940501e0
shrflah96 <shrflah96@gmail.com>
2023-02-21 12:59:07 +0330

    fix :  some bugs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
-
-
-
-
+//for back-to-top button
     $(window).scroll(function () {
         var showAfter = 800;
         if ($(this).scrollTop() > showAfter) {
@@ -18,14 +15,17 @@ $(document).ready(function() {
         return false;
     });
 
+//for menu
+
 
     $('.close-menu').click(function () {
         $('.mobile-header-nav').fadeToggle()
         $('.overlay').fadeToggle()
     })
-    $('.mobile-header__close').click(function () {
+    $('.mobile-header__close').click(function (e) {
+        e.stopPropagation()
         $('.mobile-header-nav').fadeToggle()
-        $('.overlay').fadeToggle()
+        $('.box-cart').fadeOut()
     })
 
     $('.mobile-categories-title').click(function () {
@@ -41,24 +41,37 @@ $(document).ready(function() {
         $('.menu-list').css('display', 'block')
         $('.menu-category').css('display', 'none')
 
-
     })
 
     $('.menu-title').click(function () {
-        // $(this).parent().addClass('active')
+
         $(this).parent().find('ul.menu-dropdown').toggle()
         $(this).find('.menu-upicon').toggle()
         $(this).find('.menu-downicon').toggle()
 
     })
 
+//for cart-wrapper
+
     $('.cart-wrapper').click(function (e) {
         e.stopPropagation()
-        $('.box-cart').fadeToggle()
-        $('.overlay').fadeToggle()
+        $('.box-cart').fadeIn()
+        $('.mobile-header-nav').fadeOut()
     })
+
     $('.close-cart').click(function () {
         $('.box-cart').fadeToggle()
-        $('.overlay').fadeToggle()
+    })
+
+    $('.box-cart').click(function (e) {
+        e.stopPropagation()
+    })
+    $('.mobile-header-nav ').click(function (e) {
+        e.stopPropagation()
+    })
+
+    $('body').click(function () {
+        $('.box-cart').fadeOut()
+        $('.mobile-header-nav').fadeOut()
     })
 })
